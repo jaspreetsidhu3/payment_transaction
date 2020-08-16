3d43d0b462309fb37af0981fe485b385873b3e30
jaspreetsidhu3 <jaspreetsidhu3may@gmail.com>
2020-08-16 13:56:54 +0530

    Added shared preferences for key/id aka file name

--- app/src/main/java/com/example/paymenttransaction/Fragment/AddDialogFragment.java
+++ app/src/main/java/com/example/paymenttransaction/Fragment/AddDialogFragment.java
@@ -2,10 +2,15 @@ package com.example.paymenttransaction.Fragment;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,17 +19,46 @@ import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.example.paymenttransaction.R;
 
+import java.io.File;
+
 public class AddDialogFragment extends AppCompatDialogFragment {
+    private TextView textamount;
+    AlertDialog.Builder builder;
+    View view;
+    private String txtamount;
+    public AddDialogFragment(String textamount) {
+        txtamount=textamount;
+    }
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.adddialogfragment, null);
+         builder = new AlertDialog.Builder(getContext());
+         view = LayoutInflater.from(getContext()).inflate(R.layout.adddialogfragment, null);
+            creating_dialogview();
+            textamount=view.findViewById(R.id.passamount);
+            textamount.setText(txtamount);
+         return builder.create();
+    }
+    public void creating_dialogview(){
         builder.setView(view)
-                .setTitle("Detail Dialog")
+                .setTitle("Fill transaction details")
                 .setPositiveButton("Save", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences sharedPreferences=getContext().getSharedPreferences("id_generator", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor=sharedPreferences.edit();
+                        String key=sharedPreferences.getString("file_created","-1");
+                        assert key != null;
+                        if(key.equals("-1")){
+                            Log.d("test","-1");
+                        }
+                        else{
+                            int count=Integer.parseInt(key);
+                            Toast.makeText(getContext(),""+count,Toast.LENGTH_SHORT).show();
+                            editor.putString("file_created",String.valueOf(++count));
+                            editor.apply();
+                        }
 
                     }
                 }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -33,6 +67,6 @@ public class AddDialogFragment extends AppCompatDialogFragment {
 
             }
         });
-        return builder.create();
     }
 }
+
--- app/src/main/java/com/example/paymenttransaction/Fragment/Addfragment.java
+++ app/src/main/java/com/example/paymenttransaction/Fragment/Addfragment.java
@@ -1,10 +1,17 @@
 package com.example.paymenttransaction.Fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,16 +21,38 @@ import com.example.paymenttransaction.R;
 
 public class Addfragment extends Fragment{
 private Button btnproceeds;
+private EditText fillamount;
+static int count=0;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=LayoutInflater.from(getContext()).inflate(R.layout.addfragment,null);
         btnproceeds=view.findViewById(R.id.proceed);
+        fillamount=view.findViewById(R.id.fillamount);
+        SharedPreferences sharedPreferences=getContext().getSharedPreferences("id_generator", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+
+        String key=sharedPreferences.getString("file_created","-1");
+        assert key != null;
+        if(key.equals("-1")){
+            Toast.makeText(getContext(),"Something please restart",Toast.LENGTH_SHORT).show();
+            editor.putString("file_created","-0");
+        }
+        else{
+            Log.d("test","test");
+        }
+        editor.apply();
         btnproceeds.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AddDialogFragment addDialogFragment=new AddDialogFragment();
-                addDialogFragment.show(getFragmentManager(),"Detail dialog");
+                if(TextUtils.isEmpty(fillamount.getText())){
+                    Toast.makeText(getContext(),"Please fill amount for further proceeder",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    AddDialogFragment addDialogFragment=new AddDialogFragment(fillamount.getText().toString());
+                    addDialogFragment.show(getFragmentManager(),"Detail dialog");
+                }
+
             }
         });
         return view;
