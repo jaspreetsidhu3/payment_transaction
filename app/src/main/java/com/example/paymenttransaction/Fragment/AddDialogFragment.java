741505f7e0147ac2bc15a021571f155aab8eb2f8
jaspreetsidhu3 <jaspreetsidhu3may@gmail.com>
2020-08-16 17:15:15 +0530

    added data entries

--- app/src/main/java/com/example/paymenttransaction/Fragment/AddDialogFragment.java
+++ app/src/main/java/com/example/paymenttransaction/Fragment/AddDialogFragment.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,9 +22,19 @@ import androidx.appcompat.app.AppCompatDialogFragment;
 import com.example.paymenttransaction.R;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class AddDialogFragment extends AppCompatDialogFragment {
     private TextView textamount;
+    private RadioButton radioPay;
+    private RadioButton radioRecieve;
+    private EditText edittofrom;
+    private EditText edittitle;
+    private EditText editdescription;
+    int count;
     AlertDialog.Builder builder;
     View view;
     private String txtamount;
@@ -35,8 +47,9 @@ public class AddDialogFragment extends AppCompatDialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
          builder = new AlertDialog.Builder(getContext());
          view = LayoutInflater.from(getContext()).inflate(R.layout.adddialogfragment, null);
-            creating_dialogview();
-            textamount=view.findViewById(R.id.passamount);
+            init();
+         creating_dialogview();
+
             textamount.setText(txtamount);
          return builder.create();
     }
@@ -54,10 +67,40 @@ public class AddDialogFragment extends AppCompatDialogFragment {
                             Log.d("test","-1");
                         }
                         else{
-                            int count=Integer.parseInt(key);
+                            count=Integer.parseInt(key);
                             Toast.makeText(getContext(),""+count,Toast.LENGTH_SHORT).show();
                             editor.putString("file_created",String.valueOf(++count));
                             editor.apply();
+                        }
+                        String paymenttype=null;
+                        if(radioPay.isChecked()){
+                            paymenttype="Pay";
+                        }
+                        if(radioRecieve.isChecked()){
+                            paymenttype="Recieved";
+                        }
+                        String tofrom=edittofrom.getText().toString();
+                        String title=edittitle.getText().toString();
+                        String desc=editdescription.getText().toString();
+
+                        FileOutputStream fileOutputStream= null;
+                        try {
+                            fileOutputStream = getContext().openFileOutput(count+".txt", Context.MODE_PRIVATE);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        String data="Title = "+title +"\nAmount paid = "+txtamount+"\nto/from = "+tofrom+"\n description = "+desc;
+                        try {
+                            assert fileOutputStream != null;
+                            fileOutputStream.write(data.getBytes());
+                            Toast.makeText(getContext(),""+data,Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            fileOutputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
 
                     }
@@ -67,6 +110,14 @@ public class AddDialogFragment extends AppCompatDialogFragment {
 
             }
         });
+    }
+    public void init(){
+        textamount=view.findViewById(R.id.passamount);
+        radioPay=view.findViewById(R.id.pay);
+        radioRecieve=view.findViewById(R.id.recieve);
+        edittofrom=view.findViewById(R.id.tofrom);
+        edittitle=view.findViewById(R.id.wanttoaddtitle);
+        editdescription=view.findViewById(R.id.wanttoadddescription);
     }
 }
 
